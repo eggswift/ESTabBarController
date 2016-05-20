e5ba026562f8152105786b709fe05c35fb6418de
李昊 <lihao_iOS@hotmail.com>
2016-05-20 13:50:12 +0800

    Add file ESTabBarController-swift.h

---
+++ ESTabBarControllerExample/ESTabBarController/ESTabBarController-swift.h
@@ -0,0 +1,17 @@
+//
+//  ESTabBarController-swift.h
+//  ESTabBarControllerExample
+//
+//  Created by lihao on 16/5/20.
+//  Copyright © 2016年 Egg Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Haneke.
+FOUNDATION_EXPORT double ESTabBarControllerVersionNumber;
+
+//! Project version string for Haneke.
+FOUNDATION_EXPORT const unsigned char ESTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESTabBarController/PublicHeader.h>
