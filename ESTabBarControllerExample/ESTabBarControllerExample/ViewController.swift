6ee0d99f748e0632ded28d0d461f0f21e369d6f9
Vincent Li <lihao_ios@hotmail.com>
2017-02-16 17:03:51 +0800

    Add chinese subtitles.

--- ESTabBarControllerExample/ESTabBarControllerExample/ViewController.swift
+++ ESTabBarControllerExample/ESTabBarControllerExample/ViewController.swift
@@ -12,12 +12,13 @@ public class ViewController: UIViewController, UITableViewDataSource, UITableVie
     
     @IBOutlet weak var tableView: UITableView!
     
-	public let titleArray = ["Basic", "Embed", "Animation", "Irregular", "Customize click", "Remind", "Lottie"]
-	public let dataArray = [
+	public let sectionTitleArray = ["Basic", "Embed", "Animation", "Irregular", "Customize click", "Remind", "Lottie"]
+    public let sectionSubtitleArray = ["基本", "嵌套", "动画", "不规则", "自定义点击", "提醒", "Lottie"]
+	public let titleArray = [
 		[
-			"UITabBarController default style",
-			"ESTabBarController default style",
-			"Mix ESTabBar and UITabBar ",
+			"UITabBarController style",
+			"ESTabBarController like system style",
+			"Mix ESTabBar and UITabBar",
 			"UITabBarController style with 'More'",
 			"ESTabBarController style with 'More'",
 			"Mix ESTabBar and UITabBar with 'More'",
@@ -53,33 +54,72 @@ public class ViewController: UIViewController, UITableViewDataSource, UITableVie
 			"Lottie",
 			],
 		]
-	
+
+    public let subtitleArray = [
+        [
+            "UITabBarController样式",
+            "ESTabBarController仿系统样式",
+            "ESTabBar和UITabBar混合样式",
+            "带有'More'的UITabBarController样式",
+            "带有'More'的ESTabBarController样式",
+            "带有'More'的ESTabBar和UITabBar混合样式",
+            "默认index非0的UITabBarController样式",
+            "默认index非0的ESTabBarController样式"
+        ],
+        [
+            "UINavigationController内嵌UITabBarController样式",
+            "UITabBarController内嵌UINavigationController样式",
+            ],
+        [
+            "自定义选中颜色样式",
+            "弹簧动画样式",
+            "背景颜色变化样式",
+            "带有选中效果样式",
+            "暗示用户点击样式",
+            ],
+        [
+            "中间带有较大按钮样式",
+            ],
+        [
+            "劫持按钮的点击事件",
+            "添加一个特殊的提醒框",
+            ],
+        [
+            "系统提醒样式",
+            "仿系统提醒样式",
+            "带动画提醒样式",
+            "带动画提醒样式(2)",
+            "自定义提醒样式",
+            ],
+        [
+            "Lottie",
+            ],
+        ]
+    
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.init(white: 245.0 / 255.0, alpha: 1.0)
         self.navigationItem.title = "Example"
-        if tableView != nil {
-            tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "UITableViewCell")
-        }
     }
     
     // MARK: UITableViewDataSource
     public func numberOfSections(in tableView: UITableView) -> Int {
-        return dataArray.count
+        return sectionTitleArray.count
     }
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray[section].count
+        return titleArray[section].count
     }
     
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 52.0
+        return 68.0
     }
     
     public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 42.0
     }
     public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return titleArray[section]
+        return sectionTitleArray[section] + " " + "(" + sectionSubtitleArray[section] + ")"
     }
     
     public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -87,12 +127,20 @@ public class ViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")
-        cell?.textLabel?.textColor = UIColor.init(white: 0.0, alpha: 0.6)
-        cell?.textLabel?.font = UIFont.init(name: "ChalkboardSE-Bold", size: 14.0)
-        cell?.textLabel?.text = "\(indexPath.section + 1).\(indexPath.row + 1) \(dataArray[indexPath.section][indexPath.row])"
-        cell?.textLabel?.numberOfLines = 2
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell") ?? UITableViewCell.init(style: .subtitle, reuseIdentifier: "UITableViewCell")
+
+        cell.textLabel?.textColor = UIColor.init(white: 0.0, alpha: 0.6)
+        cell.textLabel?.font = UIFont.init(name: "ChalkboardSE-Bold", size: 14.0)
+        cell.textLabel?.lineBreakMode = .byCharWrapping
+        cell.textLabel?.text = "\(indexPath.section + 1).\(indexPath.row + 1) \(titleArray[indexPath.section][indexPath.row])"
+        cell.textLabel?.numberOfLines = 2
+
+        cell.detailTextLabel?.textColor = UIColor.init(white: 0.0, alpha: 0.5)
+        cell.detailTextLabel?.font = UIFont.init(name: "ChalkboardSE-Bold", size: 11.0)
+        cell.detailTextLabel?.text = "\(indexPath.section + 1).\(indexPath.row + 1) \(subtitleArray[indexPath.section][indexPath.row])"
+        cell.detailTextLabel?.numberOfLines = 2
+
+        return cell
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
