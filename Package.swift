a8d84a3ebe7828069673ac3bc49b02c55add205e
Mohammad Porooshani <porooshani@gmail.com>
2020-01-19 10:27:30 +0330

    Updated package minfest to swift 5
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,15 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ESTabBarController",
+    platforms: [
+        .iOS(.v8),
+    ],
     products: [
         .library(name: "ESTabBarController",  targets: ["ESTabBarController"])
     ],
     dependencies: [],
     targets: [
         .target(name: "ESTabBarController", path: "Sources")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
